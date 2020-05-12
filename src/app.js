41c126b4464eca36cb37e3c2046f81544d465f97
Paulo Rocha <paulorocha1988@gmail.com>
2020-05-12 19:16:34 +0100

    getting the api back

--- src/app.js
+++ src/app.js
@@ -4,7 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
     new Vue({
         el: "#app",
         data: {
-            
+            currency: []
+        },
+        mounted(){
+            this.getCurrency()
+        },
+        methods: {
+            getCurrency: function() {
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(response => response.json())
+                .then(data => this.currency = data)
+            }
         }
-    })
-})
+    });
+    
+});
